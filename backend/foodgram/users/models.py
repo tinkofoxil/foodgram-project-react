c0de6263129b9bfcac1db8613852e01ad148b085
Герман Кабачков <german_kabachkov@mail.ru>
2022-12-04 15:57:47 +0300

    edited

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,7 +1,36 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-User = get_user_model()
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль'
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Электронная почта'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+
+    class Meta:
+        ordering = ('username',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
 
 class Follow(models.Model):
